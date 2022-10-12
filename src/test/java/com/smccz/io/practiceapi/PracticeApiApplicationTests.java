d9b22987133bfb631afba0f65ad634b10d0fa2f6
SMCCZ <sabir.mondal@cloudcraftz.io>
2022-10-12 12:00:24 +0530

    Project Created

---
+++ src/test/java/com/smccz/io/practiceapi/PracticeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smccz.io.practiceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
